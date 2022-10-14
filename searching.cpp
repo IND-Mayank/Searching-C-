5844af7199077ea03dfa589fb0f8aefd25ab9c94
IND-Mayank <87913068+IND-Mayank@users.noreply.github.com>
2022-10-14 23:39:50 +0530

    Add files via upload

---
+++ searching.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int linearsearch(int array[],int n,int key){
+	for(int i=1;i<=n;i++){
+		if(array[i]==key){
+			return i;
+		}
+	}return -1;
+}
+int main(){
+	int n;
+	cin>>n;
+	int array[n];
+	for(int i=1;i<=n;i++){
+		cin>>array[i];
+	}
+	int key;
+	cin>>key;
+	cout<<linearsearch(array,n,key);
+}
